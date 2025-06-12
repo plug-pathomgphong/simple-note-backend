243265238691b1975c788012e2b43b479782c214
plug <plug185@gmail.com>
2025-06-13 00:28:17 +0700

    chore(env): update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NotesModule],
+  imports: [NotesModule, ConfigModule.forRoot({
+    isGlobal: true, // Makes the configuration available globally
+    envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`, // Loads environment variables from a specific file based on NODE_ENV
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
