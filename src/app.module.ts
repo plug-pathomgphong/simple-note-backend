78def2f1afddb34daad943fd08e27cb6591723ca
plug <plug185@gmail.com>
2025-06-15 00:11:34 +0700

    feat(note): generate and implement prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [NotesModule, ConfigModule.forRoot({
     isGlobal: true, // Makes the configuration available globally
     envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`, // Loads environment variables from a specific file based on NODE_ENV
-  })],
+  }), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
