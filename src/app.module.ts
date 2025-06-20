1588e3e4606e97f80835607e161fe7c82165c514
plug <plug185@gmail.com>
2025-06-21 00:35:01 +0700

    feat(note): refactor code and implement s3 service forupload file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,19 @@ import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { S3Module } from './s3/s3.module';
 
 @Module({
-  imports: [NotesModule, ConfigModule.forRoot({
-    isGlobal: true, // Makes the configuration available globally
-    envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`, // Loads environment variables from a specific file based on NODE_ENV
-  }), PrismaModule],
+  imports: [
+    NotesModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration available globally
+      envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`, // Loads environment variables from a specific file based on NODE_ENV
+    }),
+    PrismaModule,
+    S3Module,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { NotesController } from './notes.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, S3Module], // Importing S3Module to use S3Service
   controllers: [NotesController],
   providers: [NotesService],
 })
