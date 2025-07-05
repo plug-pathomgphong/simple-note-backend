8ce6f29e38e681a5e0ab5912857e9a5e51b59967
plug <plug185@gmail.com>
2025-07-05 19:08:49 +0700

    Restrict CORS origin to the frontend URL for enhanced security

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'http://simple-note-frontend.s3-website-us-east-1.amazonaws.com',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   });
