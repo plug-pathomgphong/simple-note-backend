53924afcb01c5e50ada9734514647ede06a1348a
plug <plug185@gmail.com>
2025-08-06 00:40:47 +0700

    feat: implement TimeoutInterceptor for request timeouts in NotesController

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,8 @@ import { NotesModule } from './notes/notes.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { S3Module } from './s3/s3.module';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+
 import { APP_GUARD } from '@nestjs/core';
-import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -17,17 +16,8 @@ import { ScheduleModule } from '@nestjs/schedule';
     }),
     PrismaModule,
     S3Module,
-    ThrottlerModule.forRoot({
-      throttlers: [
-        {
-          ttl: 60000,
-          limit: 10,
-        },
-      ],
-    }),
-    ScheduleModule.forRoot(), // Enables scheduling capabilities
   ],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
+  providers: [AppService],
 })
 export class AppModule {}
